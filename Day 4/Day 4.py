e52a3929dc42b4a5ae3e1b5ac3d630544dcff381
Espiobest <59823894+Espiobest@users.noreply.github.com>
2022-12-04 14:19:34 -0500

    Update Day4

--- Day 4/Day 4.py
+++ Day 4/Day 4.py
@@ -13,7 +13,6 @@ for i in c:
     r2 = set(range(q, w + 1))
     x += q >= s and w <= e or s >= q and e <= w
 
-
 print(x)
 
 # Part 2
@@ -21,23 +20,12 @@ print(x)
 x = 0
 
 for i in c:
-    found = False
     a, b = i.split(",")
     s, e = map(int, a.split("-"))
     q, w = map(int, b.split("-"))
     r1 = set(range(s, e + 1))
     r2 = set(range(q, w + 1))
-
-    for j in r1:
-        if j in r2:
-            x += 1
-            found = True
-            break
-    if not found:
-        for k in r2:
-            if k in r1:
-                x += 1
-                break
-
+    if r1.intersection(r2) or r2.intersection(r1):
+        x += 1
 
 print(x)
