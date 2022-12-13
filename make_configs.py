717246a0510183bbe716f42b7ee3d604c924690a
Espiobest <59823894+Espiobest@users.noreply.github.com>
2022-12-13 02:04:17 -0500

    Update config generator to prevent errors

--- make_configs.py
+++ make_configs.py
@@ -1,7 +1,10 @@
 import os
 for i in range(1, 26):
-    os.mkdir(f"Day {i}")
-    template = '''with open('data.txt', 'r') as r:
-    c = r.read().strip().split("\\n\\n")'''
-    open(os.path.join(f'Day {i}', f'Day {i}.py'), 'w').write(template)
-    open(os.path.join(f'Day {i}', f'data.txt'), 'w').close()
+    if f"Day {i}" not in os.listdir():
+        os.mkdir(f"Day {i}")
+        template = '''with open('data.txt', 'r') as r:
+        c = r.read().strip().split("\\n\\n")'''
+        open(os.path.join(f'Day {i}', f'Day {i}.py'), 'w').write(template)
+        open(os.path.join(f'Day {i}', f'data.txt'), 'w').close()
+
+print("Created folders!")
